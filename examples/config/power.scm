4d2bfa8d4b9cd82c40b9e5a1aacb1a07d1810fde
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2012-11-16 20:39:54 +0200

    examples: power plugin stub
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

--- examples/config/power.scm
+++ examples/config/power.scm
@@ -1,4 +1,4 @@
-(plugin "power" "/usr/lib/statefs/libpower.so"
+(plugin "power" "./examples/src/libpower.so"
         ; power interface
         (ns "battery"
             (prop "voltage" 3.8 :analog)
