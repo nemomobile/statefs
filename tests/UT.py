40f2359eff079a47a952199e0c0219dfd66a550e
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2013-01-20 13:52:08 +0200

    several lines of code are reused from std unittest.py, insert copyright
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

--- tests/UT.py
+++ tests/UT.py
@@ -21,6 +21,24 @@
 
 # http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
 
+# Some code was taken from Python 2.6 unittest.py. Copyright is following.
+
+# Copyright (c) 1999-2003 Steve Purcell
+# This module is free software, and you may redistribute it and/or modify
+# it under the same terms as Python itself, so long as this copyright message
+# and disclaimer are retained in their original form.
+
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+# SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OF
+# THIS CODE, EVEN IF THE AUTHOR HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+# DAMAGE.
+
+# THE AUTHOR SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE.  THE CODE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS,
+# AND THERE IS NO OBLIGATION WHATSOEVER TO PROVIDE MAINTENANCE,
+# SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
 import sys
 import traceback
 import re
