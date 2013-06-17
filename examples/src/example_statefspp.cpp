e86d1da3cda5099e1c3332aec94b272825a4bfb7
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2013-06-17 16:50:37 +0300

    [examples] Time.Seconds property
    To use it for testing polling with different intervals
    
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- examples/src/example_statefspp.cpp
+++ examples/src/example_statefspp.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <statefs/provider.hpp>
+#include <chrono>
+#include <future>
 
 struct BeatHandle { std::string v; };
 
@@ -72,6 +74,90 @@ public:
     virtual void release() { std::cerr << "release  " << get_name() << std::endl;}
 };
 
+class Time
+{
+public:
+    Time(statefs::AProperty *parent)
+        : update_interval_usec_(1000000)
+        , parent_(parent)
+        , v_(calc())
+        , slot_(nullptr) {}
+
+    int getattr() const {
+        return STATEFS_ATTR_READ
+            | STATEFS_ATTR_DISCRETE
+            | STATEFS_ATTR_WRITE;
+    }
+    ssize_t size() const { return 128 ; }
+
+    bool connect(::statefs_slot *slot) {
+        slot_ = slot;
+        std::cerr << "connect to seconds\n";
+        periodic_update();
+        return true;
+    }
+
+    int read(BeatHandle *h, char *dst, size_t len, off_t off)
+    {
+        if (!slot_)
+            v_ = calc();
+
+        auto &v = h->v;
+        if (!off)
+            v = v_;
+
+        return read_from(v, dst, len, off);
+    }
+
+    int write(BeatHandle *h, char const *src, size_t len, off_t off)
+    {
+        update_interval_usec_ = stoi(std::string(src, len));
+        return len;
+    }
+
+    void disconnect() { slot_ = nullptr; }
+    void release() {}
+
+private:
+
+    std::string calc()
+    {
+        auto i = ::time(nullptr);
+        std::stringstream ss; ss << i;
+        return ss.str();
+    }
+
+    void periodic_update() {
+        v_ = calc();
+        if (slot_) {
+            std::cerr << "periodic\n";
+            std::thread([this]() {
+                    std::cerr << "next\n";
+                    ::usleep(update_interval_usec_);
+                    auto slot = slot_;
+                    if (slot_)
+                        slot->on_changed(slot_, parent_);
+                    periodic_update();
+                }).detach();
+        }
+    }
+    unsigned update_interval_usec_;
+    statefs::AProperty *parent_;
+    std::string v_;
+    ::statefs_slot *slot_;
+};
+
+class NsTime : public statefs::Namespace
+{
+public:
+    NsTime() : Namespace("Time")
+    {
+        insert(new statefs::BasicPropertyOwner<Time, BeatHandle>("seconds"));
+    }
+
+    virtual ~NsTime() { std::cerr << "~NsTime " << get_name() << std::endl; }
+    virtual void release() { std::cerr << "release " << get_name() << std::endl;}
+};
 
 class Provider : public statefs::AProvider
 {
@@ -79,6 +165,7 @@ public:
     Provider() : AProvider("Drink") {
         insert(new NsChild("Water"));
         insert(new NsChild("Beer"));
+        insert(new NsTime());
     }
     virtual ~Provider() { std::cerr << "~Provider " << get_name() << std::endl; }
     virtual void release() {
